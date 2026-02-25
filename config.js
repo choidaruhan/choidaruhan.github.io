2470ab7f3dc984c9a825b86d098c75fe779cdbe9
choidaruhan <chl11wq12@gmail.com>
2026-02-25 22:22:27 +0900

    chore: update blog configuration.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // TODO: 이곳에 회원님 본인의 Supabase Project URL과 anon key를 직접 입력해주세요!
 // URL은 'https:// ~ .supabase.co' 형식이어야 합니다.
 
-window.SUPABASE_URL = '여기에_URL을_입력하세요';
-window.SUPABASE_ANON_KEY = '여기에_ANON_KEY를_입력하세요';
+window.SUPABASE_URL = 'https://nzxotasyhjaguolkjvaz.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_KEGsTqhZ5bPrPEcJbaWyHA_DufmQdqo';
