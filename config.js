c2c2da4923aac7fd905171333f33d03c5d0bd60d
choidaruhan <chl11wq12@gmail.com>
2026-02-25 22:20:41 +0900

    fix: enforce manual config.js template

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-window.SUPABASE_URL = '';
-window.SUPABASE_ANON_KEY = '';
+// TODO: 이곳에 회원님 본인의 Supabase Project URL과 anon key를 직접 입력해주세요!
+// URL은 'https:// ~ .supabase.co' 형식이어야 합니다.
+
+window.SUPABASE_URL = '여기에_URL을_입력하세요';
+window.SUPABASE_ANON_KEY = '여기에_ANON_KEY를_입력하세요';
